66cf2a738ed7b22b289a0abb597871e8ff16d713
Sabas <smma1992@gmail.com>
2022-01-20 11:30:55 -0700

    created react app, deleted initial content and added the basics components that will be needed for the proyect, using react-router-dom for navigating and added css for basic core styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/layout/header";
+import Landing from "./components/pages/Landing";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Routes>
+          <Route exact path="/" element={<Landing />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/pages/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <section className="pageLimiter" id="landingPage">
+      this is the landing page lorem ipsum dolor sit amet consectetur
+      adipisicing elit.
+    </section>
+  );
+};
+
+export default Landing;
